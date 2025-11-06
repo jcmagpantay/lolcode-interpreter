d1c97234886d9cd38ad2b17dbc56b6cf9c1a2450
jcmagpantay <jcmagpantay@up.edu.ph>
2025-11-06 09:24:57 +0800

    feat: Add lexical analysis base infrastructure

--- lexer.py
+++ lexer.py
@@ -1,5 +1,7 @@
 # Python lexer
 
+import re
+
 
 # Reads the file to be lexically analyzed
 def read_file(directory) -> str:
@@ -10,15 +12,102 @@ def read_file(directory) -> str:
         directory (str): Path of the file.
 
     Returns:
-        str: File contents as string.
+        list: File contents as list of string per line.
     """ 
 
     # Error safety check
 
     with open(directory) as f:
-      print(f.read())
+        file_content = f.read()
+        lines = file_content.split('\n')
+
+        for line in lines:
+            lexeme_line = lexify(line)
+
+    return lines
+
+def lex(directory):
+    """
+    Top-level function for lexical analysis.
+    Calls `lexify` and `read_file` to perform lexing line-by-line
+
+    Args:
+        directory (str): The file to be lexified.
+
+    Returns:
+        list: A lexeme table containing tuple pairs of format: (match, pair).
+    """
+    lexeme_table = []
+
+    lines = read_file(directory)
+
+    for line in lines:
+        lexeme_line = lexify(line)
+        lexeme_table.extend(lexeme_line)
+
+    return lexeme_table
+        
+
+def lexify(line):
+    """
+    Tokenize the string line into lexemes based on RegEx patterns
+
+    Args:
+        line (str): The line to be tokenized into lexemes.
+
+    Returns:
+        list: List of tuples (match, pattern) of identified lexemes per line.
+    """
+
+    # The list to be returned
+    lexemes = []
+
+    # List of RegEx patterns to be matched against.
+    # NOTE: Order is important. Place the patterns carefully in prioritization.
+    regex_library = {
+        "KEYWORD": r"\bHAI\b",
+        "VAR_IDENTIFIER": r"[a-zA-Z][a-zA-Z0-9_]*",
+        "NUMBAR_LITERAL": r"-?[0-9]+\.[0-9]+",
+        "NUMBR_LITERAL": r"-?[0-9]+",
+        "YARN_LITERAL": "\".*\"",
+        "TROOF_LITERAL": r"WIN|FAIL",
+        "TYPE_LITERAL": r"NUMBR|NUMBAR|YARN|TROOF",
+        "IGNORE_S_T": r"[ \t\n]",
+        "NO_MATCH": r".+"
+    }
+
+    # Exhausts all patterns in library
+    for pattern in regex_library:
+
+        # Find all matches of specific pattern
+        matches = re.findall(regex_library[pattern], line)
+
+        # Then append it to the variable to be returned
+        for match in matches:
+            lexemes.append((match, pattern))
+
+        # Remove the specific pattern from line to avoid double-matching
+        line = re.sub(regex_library[pattern], '', line)
+
+    return lexemes
+
+
+def clean(token):
+    """
+    Clean the token by removing any noisy input.
+
+    Args:
+        token (str): The token to be cleaned.
+
+    Returns:
+        str: The cleaned token.
+    """
+    pass
+
 
 def main():
-    read_file("test/milestone1_test.lol")
-if __name__ == "main":
+    lexeme_table = lex("test/simple_test.lol")
+
+
+if __name__ == "__main__":
     main()
